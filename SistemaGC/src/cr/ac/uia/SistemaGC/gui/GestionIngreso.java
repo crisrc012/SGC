3709df66ab07e777faf1fb28d2ccc5cdae2509b3
pagui97 <paguimendez97@gmail.com>
2016-04-22 15:33:08 -0600

    Ingreso al comedor

--- SistemaGC/src/cr/ac/uia/SistemaGC/gui/GestionIngreso.java
+++ SistemaGC/src/cr/ac/uia/SistemaGC/gui/GestionIngreso.java
@@ -40,13 +40,18 @@ public class GestionIngreso extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btngComida = new javax.swing.ButtonGroup();
         lblTituloGI = new javax.swing.JLabel();
         lblCedPersona = new javax.swing.JLabel();
         btnAplicarIngreso = new javax.swing.JButton();
         txtCedPersona = new javax.swing.JTextField();
         jlbNombre = new javax.swing.JLabel();
         lblName = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblDescripcion = new javax.swing.JLabel();
+        lblComida = new javax.swing.JLabel();
+        rbtnDesayuno = new javax.swing.JRadioButton();
+        rbtnAlmuerzo = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
@@ -62,7 +67,7 @@ public class GestionIngreso extends javax.swing.JFrame {
         });
 
         lblTituloGI.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-        lblTituloGI.setText("Gestión de Ingreso");
+        lblTituloGI.setText("Ingreso al Comedor");
 
         lblCedPersona.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         lblCedPersona.setText("Cédula de la Persona:");
@@ -82,7 +87,21 @@ public class GestionIngreso extends javax.swing.JFrame {
 
         lblName.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Pao\\Downloads\\puerta.png")); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel2.setText("Descripción:");
+
+        lblDescripcion.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+
+        lblComida.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        lblComida.setText("Hora de comida:");
+
+        btngComida.add(rbtnDesayuno);
+        rbtnDesayuno.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        rbtnDesayuno.setText("Desayuno");
+
+        btngComida.add(rbtnAlmuerzo);
+        rbtnAlmuerzo.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        rbtnAlmuerzo.setText("Almuerzo");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -92,44 +111,59 @@ public class GestionIngreso extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(59, 59, 59)
-                        .addComponent(lblTituloGI)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel1))
+                        .addComponent(lblTituloGI))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(81, 81, 81)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblCedPersona)
-                                .addGap(28, 28, 28)
-                                .addComponent(txtCedPersona, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(149, 149, 149)
+                                .addComponent(btnAplicarIngreso))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jlbNombre)
                                 .addGap(18, 18, 18)
-                                .addComponent(lblName))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(230, 230, 230)
-                        .addComponent(btnAplicarIngreso)))
+                                .addComponent(lblName))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(133, 133, 133)
+                                .addComponent(lblDescripcion))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblCedPersona)
+                                    .addComponent(lblComida))
+                                .addGap(28, 28, 28)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(rbtnDesayuno)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(rbtnAlmuerzo))
+                                    .addComponent(txtCedPersona, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))))))
                 .addContainerGap(129, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
-                        .addComponent(lblTituloGI)))
-                .addGap(18, 18, 18)
+                .addGap(24, 24, 24)
+                .addComponent(lblTituloGI)
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblComida)
+                    .addComponent(rbtnDesayuno)
+                    .addComponent(rbtnAlmuerzo))
+                .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblCedPersona)
                     .addComponent(txtCedPersona, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(51, 51, 51)
+                .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jlbNombre)
                     .addComponent(lblName))
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(lblDescripcion))
                 .addGap(18, 18, Short.MAX_VALUE)
                 .addComponent(btnAplicarIngreso)
-                .addGap(23, 23, 23))
+                .addContainerGap())
         );
 
         pack();
@@ -141,6 +175,7 @@ public class GestionIngreso extends javax.swing.JFrame {
     }
     
     private void btnAplicarIngresoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAplicarIngresoActionPerformed
+        
         if (txtCedPersona.getText().isEmpty()) {
             JOptionPane.showMessageDialog(this, "Por favor digite una cédula válida", "Error", JOptionPane.ERROR_MESSAGE);
             return;
@@ -228,11 +263,16 @@ public class GestionIngreso extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAplicarIngreso;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.ButtonGroup btngComida;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jlbNombre;
     private javax.swing.JLabel lblCedPersona;
+    private javax.swing.JLabel lblComida;
+    private javax.swing.JLabel lblDescripcion;
     private javax.swing.JLabel lblName;
     private javax.swing.JLabel lblTituloGI;
+    private javax.swing.JRadioButton rbtnAlmuerzo;
+    private javax.swing.JRadioButton rbtnDesayuno;
     private javax.swing.JTextField txtCedPersona;
     // End of variables declaration//GEN-END:variables
 }
